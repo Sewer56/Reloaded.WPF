3375f08a0b016ac87b9baa0fefac51243f4a14f3
Sewer56 <seweratwork@gmail.com>
2019-04-24 05:09:20 +0100

    [Converters] Add Double to Grid Length Converter

---
+++ Source/Reloaded.WPF.Theme.Default/Converter/DoubleToGridLengthConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Reloaded.WPF.Theme.Default.Converter
+{
+    /// <summary>
+    /// Converts a <see cref="System.Double"/> to a <see cref="GridLength"/>.
+    /// </summary>
+    public class DoubleToGridLengthConverter : IValueConverter
+    {
+        public static DoubleToGridLengthConverter Instance = new DoubleToGridLengthConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double)
+                return new System.Windows.GridLength((double)value);
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
