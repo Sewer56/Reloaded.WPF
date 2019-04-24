58a22697cedf74c3df58d3a98a0d1440065de43a
Sewer56 <seweratwork@gmail.com>
2019-04-24 05:07:28 +0100

    [Attached Property] Add ShowComboboxArrow property.

---
+++ Source/Reloaded.WPF.Theme.Default/AttachedProperties/ShowComboboxArrow.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Reloaded.WPF.MVVM;
+
+namespace Reloaded.WPF.Theme.Default.AttachedProperties
+{
+    /// <summary>
+    /// Dependency property which if set to false, does not show the <see cref="ComboBox"/> arrow.
+    /// </summary>
+    /// <remarks>This property has no logic. All logic is done in binding.</remarks>
+    public class ShowComboboxArrow : AttachedPropertyBase<ShowComboboxArrow, bool>
+    {
+
+    }
+}
