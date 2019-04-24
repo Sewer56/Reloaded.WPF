94c4312a9e0738ac023ce4a6a1c25ddc6447db66
Sewer56 <seweratwork@gmail.com>
2019-04-24 05:06:05 +0100

    Misc Documentation Improvements

--- Source/Reloaded.WPF/Controls/BindingProxy.cs
+++ Source/Reloaded.WPF/Controls/BindingProxy.cs
@@ -13,9 +13,10 @@ namespace Reloaded.WPF.Controls
     /// The <see cref="BindingProxy"/> is a <see cref="Freezable"/> <see cref="DependencyObject"/> of the WPF
     /// framework that does only one thing: Store arbitrary data that can be binded to.
     /// 
-    /// This allows us to use this class, as a proxy to bind to elements that are not explicitly
-    /// part of the WPF framework such as <see cref="Color"/>, while still being able to gain the full
-    /// power of e.g. <see cref="IValueConverter"/>s.
+    /// This allows us to use this class as a proxy to bind to arbitrary elements
+    /// which may or may not be explicitly be part of the WPF framework, while still 
+    /// being able to use the full power of WPF's binding system such as 
+    /// <see cref="IValueConverter"/>s.
     ///
     /// To use this class, simply make a key(ed) instance of this class in the resources of any WPF element
     /// such as a window or control.
