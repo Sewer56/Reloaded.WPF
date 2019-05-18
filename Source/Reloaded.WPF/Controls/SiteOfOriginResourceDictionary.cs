c1e3ab1c6560f269384332404e32caf752c84357
Sewer56 <seweratwork@gmail.com>
2019-05-18 13:08:09 +0100

    Add "Site of Origin" Resource Dictionary

---
+++ Source/Reloaded.WPF/Controls/SiteOfOriginResourceDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reloaded.WPF.Controls
+{
+    /// <summary>
+    /// A modified type of <see cref="ResourceDictionary"/> that translates "Resource" pack URIs to "Site of Origin"
+    /// pack URIs when in runtime.
+    ///
+    /// i.e. This allows you to declare pack URIs as "pack://application:,,,", which will be resolved
+    /// as such in design mode while at runtime, actually using "pack://siteoforigin:,,,".
+    /// </summary>
+    public class SiteOfOriginResourceDictionary : ResourceDictionary
+    {
+        private const string SiteOfOriginPrefix = "pack://siteoforigin:,,,";
+        private const string ApplicationPrefix = "pack://application:,,,";
+
+        private const string UseRedirectSource = "Please use RedirectSource instead of Source";
+        private string _originalUri;
+
+        /// <summary>
+        /// Gets or sets the design time source.
+        /// </summary>
+        public string RedirectSource
+        {
+            get => _originalUri;
+
+            set
+            {
+                this._originalUri = value;
+                bool isInDesignMode = (bool) DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue;
+
+                if (! isInDesignMode)
+                {
+                    if (value.Contains(ApplicationPrefix))
+                        value = value.Replace(ApplicationPrefix, SiteOfOriginPrefix);
+                }
+
+                base.Source = new Uri(value);
+            }
+        }
+
+        /// <summary>
+        /// Please use <see cref="RedirectSource"/>
+        /// </summary>
+        public new Uri Source
+        {
+            get => throw new Exception(UseRedirectSource);
+            set => throw new Exception(UseRedirectSource);
+        }
+    }
+}
